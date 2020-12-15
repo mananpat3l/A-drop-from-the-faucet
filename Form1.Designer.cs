b49fbeab0cc2c527a71b49f278ca9a5fe8826ef1
Manan Patel <74943831+mananpat3l@users.noreply.github.com>
2020-12-15 02:22:55 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Lab5a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.color_button = new System.Windows.Forms.Button();
+            this.closebutton1 = new System.Windows.Forms.Button();
+            this.quantity_trackBar = new System.Windows.Forms.TrackBar();
+            this.tappictureBox = new System.Windows.Forms.PictureBox();
+            this.ticktimer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.quantity_trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tappictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // color_button
+            // 
+            this.color_button.BackColor = System.Drawing.SystemColors.Control;
+            this.color_button.Location = new System.Drawing.Point(39, 12);
+            this.color_button.Name = "color_button";
+            this.color_button.Size = new System.Drawing.Size(75, 23);
+            this.color_button.TabIndex = 0;
+            this.color_button.Text = "Color";
+            this.color_button.UseVisualStyleBackColor = true;
+            this.color_button.Click += new System.EventHandler(this.color_button_Click);
+            // 
+            // closebutton1
+            // 
+            this.closebutton1.Location = new System.Drawing.Point(221, 12);
+            this.closebutton1.Name = "closebutton1";
+            this.closebutton1.Size = new System.Drawing.Size(75, 23);
+            this.closebutton1.TabIndex = 1;
+            this.closebutton1.Text = "Close";
+            this.closebutton1.UseVisualStyleBackColor = true;
+            this.closebutton1.Click += new System.EventHandler(this.closebutton1_Click);
+            // 
+            // quantity_trackBar
+            // 
+            this.quantity_trackBar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.quantity_trackBar.Location = new System.Drawing.Point(25, 73);
+            this.quantity_trackBar.Name = "quantity_trackBar";
+            this.quantity_trackBar.Size = new System.Drawing.Size(118, 45);
+            this.quantity_trackBar.TabIndex = 0;
+            this.quantity_trackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // tappictureBox
+            // 
+            this.tappictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tappictureBox.Image = ((System.Drawing.Image)(resources.GetObject("tappictureBox.Image")));
+            this.tappictureBox.Location = new System.Drawing.Point(12, 124);
+            this.tappictureBox.Name = "tappictureBox";
+            this.tappictureBox.Size = new System.Drawing.Size(251, 242);
+            this.tappictureBox.TabIndex = 2;
+            this.tappictureBox.TabStop = false;
+            // 
+            // ticktimer1
+            // 
+            this.ticktimer1.Tick += new System.EventHandler(this.ticktimer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(756, 749);
+            this.Controls.Add(this.tappictureBox);
+            this.Controls.Add(this.quantity_trackBar);
+            this.Controls.Add(this.closebutton1);
+            this.Controls.Add(this.color_button);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "A drop from bucket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.quantity_trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tappictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button color_button;
+        private System.Windows.Forms.Button closebutton1;
+        private System.Windows.Forms.TrackBar quantity_trackBar;
+        private System.Windows.Forms.PictureBox tappictureBox;
+        private System.Windows.Forms.Timer ticktimer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//I, Manan Patel, 000826892 certify that this material is my original work.  No other person's work has been used without due acknowledgement.
+
+namespace Lab5a
+{
+    /// <summary>
+    /// This is the graphical form of a bucket filling up with the tap water and by using color dialog box one can change the colour of it.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+
+        private Graphics g; //Encapsulates a GDI+ drawing surface
+        private int h;     //to keep track of height
+        private bool fill; //to check if bucket is filled or not 
+        float[] flowRate = { 0, 180, 160, 140, 120, 100, 80, 60, 40, 20, 10 }; //to incerease
+        private Color c = Color.White; //Represents a color, initially set to white
+        public Form1()
+        {
+            
+            InitializeComponent();
+            quantity_trackBar.Value = 0; //initial value is 0
+            this.Paint += new PaintEventHandler(Form1_Paint); //initializing the paint to draw bucket
+            
+        }
+        // creating the bucket with diffent dimensions.
+        private void Form1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.White), 200, 400, 200 ,620);
+            e.Graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.White), 200, 620, 400, 620);
+            e.Graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.White), 400, 620, 400, 400);
+        }
+        private void color_button_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog(); //Colordialog box is setted up
+            dlg.ShowDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                c = dlg.Color;
+            }
+
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();  //creates a graphics object
+            if (fill)   //if bucket is filled
+            {
+                g.FillRectangle(new SolidBrush(BackColor), 201, 400, 349, 400); //this will graphically display a liquid filling a bucket 
+                fill = false;
+            }
+
+            else
+            {
+                if (quantity_trackBar.Value > 0)  //if value of trackbar is greater than zero 
+                {
+                    ticktimer1.Interval = 300;
+                    ticktimer1.Interval = (int)flowRate[quantity_trackBar.Value];
+                }
+
+                else if (quantity_trackBar.Value == 0) //tap will stop if trackbar value is 0
+                {
+                    ticktimer1.Stop();
+                    ticktimer1.Stop(); //timer will stop
+                    g.FillRectangle(new SolidBrush(BackColor), 201, 200, 15, 200 - h); //this will graphically display a rectangle
+                }
+            }
+
+            ticktimer1.Start();
+        
+    }
+
+        private void closebutton1_Click(object sender, EventArgs e)
+        {
+            Close();//to exit the form
+        }
+
+       
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //to restart when it fills up completely
+
+        private void ticktimer1_Tick_1(object sender, EventArgs e)
+        {
+            Graphics graphics = CreateGraphics(); //creates a graphics object
+            graphics.FillRectangle(new SolidBrush(c), 201, 620 - h, 199, 1); //this will draw water falling form tap into bucket
+            graphics.FillRectangle(new SolidBrush(c), 210, 200, 15, 420 - h + 1);
+            h += 1;
+
+            if ((395 - h) == 180) //if height of bucket is reached
+            {
+                fill = true;
+                h = 0;  //counter set to 0 because tap was stopped
+                ticktimer1.Stop(); //timer will stop
+                quantity_trackBar.Value = 0;  //setting value of trackbar to 0
+                graphics.FillRectangle(new SolidBrush(BackColor), 210, 200, 15, 96);//resets to the blackbackground again to restart when it fills up.
+            }
+        }
+    }
+}
+//END
